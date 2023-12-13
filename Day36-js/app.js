e0e29fde6c8789ab7204ca6f738e77612e83a56e
Vishvajeet <77197069+YesiamAdarsh@users.noreply.github.com>
2023-12-13 06:36:02 +0530

    assingment java script

--- Day36-js/app.js
+++ Day36-js/app.js
@@ -1,157 +1,157 @@
-// let color = "green";
+let color = "green";
 
-// if (color == "red") {
-//     console.log("Stop");   
-// }
-// if (color == "yellow") {
-//     console.log("Ready");  
-// }
-// if (color == "green") {
-//     console.log("Go");   
-// }
-
-
-
-
-// let size = "xl";
-
-// if (size == "s") {
-//     console.log("Price: 50rs");
-// }
-// else if (size == "m") {
-//     console.log("Price: 100rs");
-// }
-// else if (size == "l") {
-//     console.log("Price: 200rs");
-// }
-// else if (size == "xl") {
-//     console.log("Price: 250rs");
-// }
+if (color == "red") {
+    console.log("Stop");   
+}
+if (color == "yellow") {
+    console.log("Ready");  
+}
+if (color == "green") {
+    console.log("Go");   
+}
 
 
 
-// let str = "addarsh";
 
-// if (str[0] === "a" && str.length > 3) {
-//     console.log("good string");
-// }
-// else {
-//     console.log("bad string");
-// }
+let size = "xl";
+
+if (size == "s") {
+    console.log("Price: 50rs");
+}
+else if (size == "m") {
+    console.log("Price: 100rs");
+}
+else if (size == "l") {
+    console.log("Price: 200rs");
+}
+else if (size == "xl") {
+    console.log("Price: 250rs");
+}
 
 
 
-// let day = 5;
+let str = "addarsh";
 
-// switch (day) {
-//     case 1:
-//         console.log("Monday");
-//         break;
-//     case 2:
-//         console.log("Tuesday");
-//         break;
-//     case 3:
-//         console.log("Wednesday");
-//         break;
-//     case 4:
-//         console.log("Thursday");
-//         break;
-//     case 5:
-//         console.log("Friday");
-//         break;
-//     case 6:
-//         console.log("Saturday");
-//         break;
-//     case 7:
-//         console.log("Sunday");
-//         break;
-//     default:
-//         console.log("Wrong Input");
-//         break;
-// }
+if (str[0] === "a" && str.length > 3) {
+    console.log("good string");
+}
+else {
+    console.log("bad string");
+}
 
-// alert("something is wrong!!!")
+
+
+let day = 5;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Wrong Input");
+        break;
+}
+
+alert("something is wrong!!!")
 
 
 
 // Assingment Questiona
 
 // question 1
-// let num = 5;
+let num = 5;
 
-// if (num%10 == 0) {
-//     console.log("good!")
-// }
-// else {
-//     console.log("bad!")
-// }
+if (num%10 == 0) {
+    console.log("good!")
+}
+else {
+    console.log("bad!")
+}
 
 // question 2
-// let Name = prompt("Enter your Name: ");
-// let age = prompt("Enter your age: ");
+let Name = prompt("Enter your Name: ");
+let age = prompt("Enter your age: ");
 
-// alert(`${Name} is ${age} years old.`);
+alert(`${Name} is ${age} years old.`);
 
 
 // question 3
-// let quater = 3;
+let quater = 3;
 
-// switch (quater) {
-//     case 1:
-//         console.log("January, February, March");
-//         break;
-//     case 2:
-//         console.log("April, May, June");
-//         break;
-//     case 3:
-//         console.log("July, August, September");
-//         break;
-//     case 4:
-//         console.log("October, November, December");
-//         break;
+switch (quater) {
+    case 1:
+        console.log("January, February, March");
+        break;
+    case 2:
+        console.log("April, May, June");
+        break;
+    case 3:
+        console.log("July, August, September");
+        break;
+    case 4:
+        console.log("October, November, December");
+        break;
 
-//     default:
-//         break;
-// }
+    default:
+        break;
+}
 
 
 // question 4
-// let str = "adarsh";
+let string = "adarsh";
 
-// if ((str[0] === "a" || str[0] === "A") && str.length > 5) {
-//     console.log("Golden string");
-// }
-// else {
-//     console.log("Not a golden string");
-// }
+if ((string[0] === "a" || string[0] === "A") && string.length > 5) {
+    console.log("Golden string");
+}
+else {
+    console.log("Not a golden string");
+}
 
 
 // question 5
-// let a = 5;
-// let b = 10;
-// let c = 3;
+let a = 5;
+let b = 10;
+let c = 3;
 
-// if (a>b && a>c) {
-//     console.log("a is greatest");
-// }
-// else if (b>a && b>c) {
-//     console.log("b is greatest");
-// }
-// else if (c>a && c>b) {
-//     console.log("c is greatest");
-// }
-// else {
-//     console.log("all are equal");
-// }
+if (a>b && a>c) {
+    console.log("a is greatest");
+}
+else if (b>a && b>c) {
+    console.log("b is greatest");
+}
+else if (c>a && c>b) {
+    console.log("c is greatest");
+}
+else {
+    console.log("all are equal");
+}
 
 
 // question 6(Bonus)
-// let num1 = 4554;
-// let num2 = 4512785;
+let num1 = 4554;
+let num2 = 4512785;
 
-// if ((num1%10)==(num2%10)) {
-//     console.log("numbers have the same last digit which is",num1%10);
-// }
-// else {
-//     console.log("numbers have different last digit");
-// }
+if ((num1%10)==(num2%10)) {
+    console.log("numbers have the same last digit which is",num1%10);
+}
+else {
+    console.log("numbers have different last digit");
+}
